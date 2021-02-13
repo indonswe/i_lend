028af561ece3f8b356cc52b0ab474be5840a2918
indonswe <afc2002afc@hotmail.com>
2021-02-13 21:23:24 +0100

    add cred pro

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,20 @@
 package org.example;
 
 
+import org.example.data.AppUserDaoJDBC;
+import org.example.model.AppUser;
+
 public class App
 {
     public static void main( String[] args ) {
+        AppUserDaoJDBC dao = new AppUserDaoJDBC();
 
+        //AppUser erik = dao.findById(1).orElseThrow(RuntimeException::new);
+        //erik.setPassword("koolKid");
+        //erik = dao.update(erik);
+
+        //System.out.println(erik.getPassword());
+
+        System.out.println(dao.findById(1));
     }
 }
